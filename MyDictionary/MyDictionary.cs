13a60e0ca62a5e98276b120d9cc06c5d339ef8cb
Oğuz Gürkan Altunok <oguzgurkan2001@gmail.com>
2021-01-21 14:12:48 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] items;
+        // constructor
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] TempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < TempArray.Length; i++)
+            {
+                items[i] = TempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+
+            isimler.Add("Gürkan");
+            isimler.Add("Engin");
+            Console.WriteLine(isimler.Length); //Test
+            isimler.Add("Ahmet");
+            isimler.Add("Murat");
+            Console.WriteLine(isimler.Length); //Test
+        }
+    }
+}
